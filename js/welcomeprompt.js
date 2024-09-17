6c7ee855fc56d52f6c3d0121e61ae976f7166134
Monica Paultre <paulmj1@mail.broward.edu>
2024-09-17 17:12:48 -0400

    Added Main Files

--- js/welcomeprompt.js
+++ js/welcomeprompt.js
@@ -1,5 +1,5 @@
 
-var input = prompt("Welcome to my Website! What's your name?")
+var input = prompt("Welcome to my website! What's your name?")
 
 var response = "Nice to meet you, " + input + ". I hope you have an amazing day!!";
 
